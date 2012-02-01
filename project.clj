ef2a3ced0e9056ad85d458a113a2b3639cb52d9b
Nicolas Buduroi <nbuduroi@gmail.com>
2012-02-01 18:08:19 -0500

    Initial commit; empty project.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject headless-chicken "0.1.0-SNAPSHOT"
+  :description "Experimental structured search engine."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/headless_chicken/test/core.clj
@@ -0,0 +1,6 @@
+(ns headless-chicken.test.core
+  (:use [headless-chicken.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
